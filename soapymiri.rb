afc94305ed0471bd887e1ef9aed8ca27ef1a8ec7
Boris Vinogradov <no111u3@gmail.com>
2023-06-15 09:59:53 +0200

    Fix branch for soapymiri

--- soapymiri.rb
+++ soapymiri.rb
@@ -1,6 +1,6 @@
 class Soapymiri < Formula
   homepage 'https://github.com/ericek111/libmirisdr-5'
-  head 'https://github.com/ericek111/SoapyMiri.git'
+  head 'https://github.com/ericek111/SoapyMiri.git', branch: "main"
 
   depends_on "pkg-config" => :build
   depends_on 'cmake' => :build
