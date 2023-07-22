a5f63dfe151ce688be9393271433216db75ffca8
Boris Vinogradov <no111u3@gmail.com>
2023-07-22 14:05:16 +0200

    Add cubicsdr package build

---
+++ cubicsdr.rb
@@ -0,0 +1,19 @@
+class CubicSdr < Formula
+  homepage 'https://github.com/cjcliffe/CubicSDR'
+  head 'https://github.com/cjcliffe/CubicSDR.git', branch: "main"
+
+  depends_on "pkg-config" => :build
+  depends_on 'cmake' => :build
+  depends_on 'libusb'
+  depends_on 'soapysdr'
+  depends_on 'wxwidgets'
+
+  def install
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DCMAKE_BUILD_TYPE=Release -DBUNDLE_APP=1 -DCPACK_BINARY_DRAGNDROP=1"
+      system 'make'
+      system 'codesign --force --deep --sign - x64/CubicSDR.app'
+      system 'make install'
+    end
+  end
+end
